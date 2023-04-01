a5aeecf50efe40630f05dfe80620f8da78abcb72
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-04-01 09:26:35 +0700

    [api] - login

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -8,19 +8,16 @@ const jwt = require("jsonwebtoken");
 class AuthController {
     async Login(req, res) {
         try {
-            const { account, password } = req.body;
-            if (validation.ValidateEmail(account)) {
-                const user = await userModel.findOne({
-                    $or: [
-                        { email: account },
-                        { username: account }
-                    ]
-                });
-                if (!user) {
-                    res.status(400).json(responseDTO.badRequest("This user does not exist"));
-                }
-                return this.LoginUser(password, user, req, res);
+            const user = await userModel.findOne({
+                $or: [
+                    { email: account },
+                    { username: account }
+                ]
+            });
+            if (!user) {
+                res.status(400).json(responseDTO.badRequest("This user does not exist"));
             }
+            return LoginUser(password, user, req, res);
         } catch (error) {
             console.log(error);
             return res.status(500).json(responseDTO.serverError(error.message));
@@ -29,26 +26,33 @@ class AuthController {
 
     async Register(req, res) {
         try {
-            const { fullname, username, email, password } = req.body;
-            const checkUser = validation.ValidaiteRegister(req.body);
-
-            // Simple validate
-            if (checkUser) {
-                return res.status(400).json(responseDTO.badRequest(checkUser))
+            const { fullname, username, email, mobile, password } = req.body;
+            if (validation.ValidateMobile(mobile)) {
+                res.status(200).json(responseDTO.success('Successfully, please check your phone!'));
             }
+            
+            if (validation.ValidateEmail(email)) {
 
-            const newUserName = username.toLowerCase().replace(/ /g, '');
+                const checkUser = validation.ValidaiteRegister(req.body);
 
-            const salt = await passwordUtil.GenerateSalt();
-            const newPassword = await passwordUtil.GeneratePassword(password, salt);
-            const newUser = {
-                fullname, username: newUserName, email, password: newPassword, salt
+                // Simple validate
+                if (checkUser) {
+                    return res.status(400).json(responseDTO.badRequest(checkUser))
+                }
+
+                const newUserName = username.toLowerCase().replace(/ /g, '');
+
+                const salt = await passwordUtil.GenerateSalt();
+                const newPassword = await passwordUtil.GeneratePassword(password, salt);
+                const newUser = {
+                    fullname, username: newUserName, email, password: newPassword, salt
+                }
+                const activeToken = await signature.GenerateActiveToken(newUser);
+                const url = `${CLIENT_URL}/active/${activeToken}`
+                const mailer = new Mailer(email, url, "Verify your email address");
+                mailer.sendMail();
+                res.status(200).json(responseDTO.success('Successfully, please check your email!'));
             }
-            const activeToken = await signature.GenerateActiveToken(newUser);
-            const url = `${CLIENT_URL}/active/${activeToken}`
-            const mailer = new Mailer(email, url, "Verify your email address");
-            mailer.sendMail();
-            res.status(200).json(responseDTO.success('Successfully, please check your email!'));
         } catch (error) {
             console.log(error);
             return res.status(500).json(responseDTO.serverError(error.message));
@@ -73,11 +77,24 @@ class AuthController {
             return res.status(500).json(responseDTO.serverError(error.message));
         }
     }
+
 }
 const LoginUser = async (password, user, req, res) => {
     try {
+        const validPassword = await passwordUtil.ValidatePassword(password, user.password, user.salt);
+        if (!validPassword) {
+            let msgErr = "";
+            msgErr = user.type === "register"
+                ? "Password is incorrect"
+                : `Password is incorrect. This account login with ${user.type}`;
+            return res.status(400).json(responseDTO.badRequest(msgErr));
+        }
+        await signature.GenerateRefreshToken({ userId: user._id }, res);
+        const access_token = await signature.GenerateAccessToken({ userId: user._id });
+
         return res.status(200).json(responseDTO.success("Logged Successfully", {
-            user: { ...user, password: "" }
+            user: { ...user._doc, password: "" },
+            access_token: access_token
         }));
     } catch (error) {
         console.log(error);
