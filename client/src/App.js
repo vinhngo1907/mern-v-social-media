fab4bfb64373d7b1cd8b035d3c8e031d2542bb16
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-04-01 16:00:12 +0700

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Login from "./pages/login";
 import Alert from "./components/alert/Alert";
 import { refreshToken } from "./redux/actions/authAction";
 import Header from "./components/header/Header";
+import PrivateRouter from "./customRouter/PrivateRouter";
+import PageRender from "./customRouter/PageRender";
 
 function App() {
 	const { auth } = useSelector(state => state);
@@ -24,6 +26,8 @@ function App() {
 				{auth.token && <Header />}
 					<Route exact path="/" component={auth.token ? Home : Login} />
 					<Route exact path="/login" component={Login} />
+					<PrivateRouter exact path="/:page" component={PageRender} />
+					<PrivateRouter exact path="/:page/:id" component={PageRender} />
 					
 				</div>
 			</div>
--- client/src/customRouter/PrivateRouter.js
+++ client/src/customRouter/PrivateRouter.js
@@ -1,9 +1,8 @@
-import React from "react";
 import { Redirect, Route } from "react-router-dom";
 
-const ProtectedRouter = (props) => {
+const PrivateRouter = (props) => {
     const firstLogin = localStorage.getItem("firstLogin");
     return firstLogin ? <Route {...props} /> : <Redirect to="/" />
 }
 
-export default ProtectedRouter;
+export default PrivateRouter;
