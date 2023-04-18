378f4c943ad6d2769f77cf00b409464abd9343c8
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-04-18 19:58:46 +0700

    carousel

---
+++ client/src/components/other/Carousel.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Carousel = ({images, id}) => {
+    const isActive = (index) => {
+        if(index === 0) return "active";
+    }
+    return (
+        <div id={`image${id}`} className="carousel slide" data-ride="carousel">
+             <ol className="carousel-indicators" style={{zIndex: 1}}>
+             {
+                    images.map((img, index) => (
+                        <li key={index} data-target={`#image${id}`} 
+                        data-slide-to={index} className={isActive(index)} />
+                    ))
+                }
+             </ol>
+             <div class="carousel-inner">
+             {
+                    images.map((img, index) => (
+                        <div key={index} className={`carousel-item ${isActive(index)}`}>
+                            {
+                                img.url.match(/video/i)
+                                ? <video controls src={img.url} className="d-block w-100" alt={img.url}
+                                style={{filter: theme ? 'invert(1)' : 'invert(0)'}} />
+
+                                : <img src={img.url} className="d-block w-100" alt={img.url}
+                                style={{filter: theme ? 'invert(1)' : 'invert(0)'}} />
+                            }
+                           
+                        </div>
+                    ))
+                }
+             </div>
+        </div>
+    )
+}
+
+export default Carousel;
