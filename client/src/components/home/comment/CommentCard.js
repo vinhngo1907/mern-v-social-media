d2ea492eeee28c4d6181d16edb077083f59d830b
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-04-23 14:55:16 +0700

    commit

---
+++ client/src/components/home/comment/CommentCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CommentCard = () =>{
+    return (
+        <></>
+    )
+}
+
+export default CommentCard;
---
+++ client/src/components/home/comment/CommentMenu.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CommentMenu = () =>{
+    return (
+        <></>
+    )
+}
+
+export default CommentMenu;
