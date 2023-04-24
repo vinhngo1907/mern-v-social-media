e1c8c4e4d53cb8a6fb26be3db2e6587b27848035
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-04-24 23:04:25 +0700

    style css

--- client/src/components/home/Views.js
+++ client/src/components/home/Views.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Avatar from "../other/Avatar";
 
 const Views = ({ auth }) => {
     return (
-        <div className="tab-pane fade show" id="link2">
+        <div className="tab-pane fade active show" id="link2">
             <span><i className="ti-eye"></i>440</span>
             <Link to="#" title="weekly-likes">440 new views this week</Link>
             <div className="users-thumb-list">
                 <Link to="#" title="" data-toggle="tooltip" data-original-title="Anderw">
-                    <img src={auth.user.avatar} alt="avatar" />
+                    <Avatar src={auth.user.avatar} size="small-avatar" />
                 </Link>
             </div>
         </div>
