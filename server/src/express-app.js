d171f86792856e6899ededfa2e580f4da71bc234
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-04-30 12:51:31 +0700

    commit

--- server/src/express-app.js
+++ server/src/express-app.js
@@ -2,10 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require("cookie-parser");
 const fileUpload = require('express-fileupload');
-const helmet = require("helmet")
+const helmet = require("helmet");
+const morgan = require("morgan");
+const logger =  require('node-color-log');
+const cron = require('cron');
+
 const WebRoute = require('./routes');
 
 module.exports = async (app) => {
+    app.enable('trust proxy');
+    app.use(helmet());
+    app.use(morgan('common'));
     app.use(express.json());
     app.use(cors({
         origin: "*"
@@ -15,7 +22,8 @@ module.exports = async (app) => {
     app.use(fileUpload({
         useTempFiles: true
     }));
-    app.use(helmet());
+    
+    const { CronJob } = cron;
     //api
     WebRoute(app);
     
