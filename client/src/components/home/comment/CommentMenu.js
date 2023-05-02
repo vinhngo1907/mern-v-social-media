5427cfac8ec3c0dd23b3e52bfa7e432eb0e8ac4f
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-05-02 15:01:43 +0700

    comment menu

--- client/src/components/home/comment/CommentMenu.js
+++ client/src/components/home/comment/CommentMenu.js
@@ -20,7 +20,29 @@ const CommentMenu = ({post, comment, setOnEdit}) =>{
         )
     }
     return (
-        <div className='menu'></div>
+        <div className='menu'>
+            {
+                (post.user._id === auth.user._id || comment.user._id === auth.user._id) &&
+                <div className="nav-item dropdown">
+                    <span className="material-icons" id="moreLink" data-toggle="dropdown">
+                        more_vert
+                    </span>
+
+                    <div className="dropdown-menu" aria-labelledby="moreLink">
+                        {
+                            post.user._id === auth.user._id
+                            ? comment.user._id === auth.user._id
+                                ? MenuItem()
+                                : <div className="dropdown-item" onClick={handleRemove}>
+                                    <span className="material-icons">delete_outline</span> Remove
+                                </div>
+                            : comment.user._id === auth.user._id && MenuItem()
+                        }
+                    </div>
+
+                </div>
+            }
+        </div>
     )
 }
 
