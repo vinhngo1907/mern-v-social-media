911c77b07df7a858efb6e6e480994787ff831e1f
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-05-07 22:24:49 +0700

    add comment

--- server/src/routes/statistic.routing.js
+++ server/src/routes/statistic.routing.js
@@ -3,6 +3,11 @@ const { StatisticController } = require("../controllers");
 const statisticCtrl = new StatisticController;
 const { userAuth } = require('../middleware');
 
+/**
+ * @route GET api/statistic
+ * @desc Get statistics
+ * @access Private
+ */
 router.get('/', userAuth, statisticCtrl.FetchAllStats);
 
 module.exports = router;
--- server/src/routes/upload.routing.js
+++ server/src/routes/upload.routing.js
@@ -4,8 +4,18 @@ const { userAuth } = require('../middleware');
 const { UploadController } = require("../controllers");
 const uploadCtrl = new UploadController();
 
+/**
+ * @route POST api/upload/create
+ * @desc Post upload file
+ * @access Private
+ */
 router.post('/create', userAuth, uploadCtrl.post);
 
+/**
+ * @route POST api/upload/destroy
+ * @desc Post destroy file
+ * @access Private
+ */
 router.post('/destroy', userAuth, uploadCtrl.delete);
 
 module.exports = router;
