85933ad48b3af24511ea5a89d29be0dc5083e332
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-05-07 22:26:28 +0700

    add comment

--- server/src/routes/comment.routing.js
+++ server/src/routes/comment.routing.js
@@ -4,10 +4,39 @@ const { userAuth } = require('../middleware');
 const { CommentController } = require("../controllers")
 const commentCtrl = new CommentController();
 
+/**
+ * @route POST api/comment
+ * @desc Post comment
+ * @access Private
+ */
 router.post("/", userAuth, commentCtrl.CreateComment);
+
+/**
+ * @route PUT api/comment
+ * @desc Put comment
+ * @access Private
+ */
 router.put("/:id", userAuth, commentCtrl.UpdateComment);
+
+/**
+ * @route PATCH api/comment
+ * @desc Patch like comment
+ * @access Private
+ */
 router.patch("/:id/like", userAuth, commentCtrl.LikeComment);
+
+/**
+ * @route PATCH api/comment
+ * @desc Patch unlike comment
+ * @access Private
+ */
 router.patch("/:id/unlike", userAuth, commentCtrl.UnLikeComment);
+
+/**
+ * @route DELETE api/comment
+ * @desc Delete comment
+ * @access Private
+ */
 router.delete("/:id", userAuth, commentCtrl.RemoveComment);
 
 module.exports = router;
