03f18dfb738c16e4f9ecafe292ea6810fcdb75a6
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-06-04 20:34:32 +0700

    [api] - create message

--- server/src/controllers/message.controller.js
+++ server/src/controllers/message.controller.js
@@ -5,9 +5,27 @@ const { modelSchema } = require("../db");
 const { conversationModel, messageModel } = modelSchema;
 
 class MessageController {
-    async createMessage(){
+    async createMessage() {
         try {
-            const updatedConversation = await conversationModel.findOneAndUpdate({});
+            const { sender, recipient, text, media, call } = req.body;
+            if (!recipient || (!text.trim() && media.length === 0 && !call)) return;
+
+            const conversation = await conversationModel.findOneAndUpdate({
+                $or: [
+                    { recipients: [sender, recipient] },
+                    { recipients: [recipient, sender] },
+                ]
+            }, {
+                recipients: [sender, recipient],
+                ...req.body
+            }, { upsert: true, new: true, runValidators: true });
+
+            const newMess = new messageModel({
+                conversation: conversation._id,
+                text, media, call, sender, recipient
+            });
+            await newMess.save();
+            res.status(200).json(responseDTO.success("Created new message in successfully", newMess))
         } catch (error) {
             console.log(error);
             return res.status(500).json(responseDTO.serverError(error.message));
