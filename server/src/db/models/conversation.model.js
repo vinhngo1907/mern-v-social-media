0aa939bd86668f7d7d57180d449d3feaf43d066f
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-06-04 10:58:37 +0700

    [model] - message, conversation

--- server/src/db/models/conversation.model.js
+++ server/src/db/models/conversation.model.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ConversationSchema = new Schema({
-    
     recipients: [{ type: Schema.Types.ObjectId, ref: 'user' }],
-
+    text: String,
+    media: Array,
+    call: Object
 }, {
     timestamps: true
 });
 
-module.exports = ConversationSchema;
+module.exports = mongoose.model("conversation", ConversationSchema);
--- server/src/db/models/message.model.js
+++ server/src/db/models/message.model.js
@@ -1,2 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+    conversation: { type: Schema.Types.ObjectId, ref: 'conversation' },
+    sender: { type: Schema.Types.ObjectId, ref: 'user' },
+    recipient: { type: Schema.Types.ObjectId, ref: 'user' },
+    text: String,
+    media: Array,
+    call: Object
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('message', messageSchema);
