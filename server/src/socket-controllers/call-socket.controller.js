7465431fc9662ea207da219b7cf60f57dca9dbcd
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-07-25 21:57:41 +0700

    ༼ つ ◕_◕ ༽つ

--- server/src/socket-controllers/call-socket.controller.js
+++ server/src/socket-controllers/call-socket.controller.js
@@ -12,7 +12,17 @@ class CallSocketContoller {
 
     startCall(io, socket, users, data) {
         try {
-
+            users = this.editData(users, data.sender, data.recipient);
+            const client = users.find(u => u.id === data.recipient);
+            if (client) {
+                if (client.call) {
+                    socket.emit("userBusy", data);
+                    users = this.editData(users, data.sender, null);
+                } else {
+                    users = this.editData(users, data.recipient, data.sender);
+                    socket.to(`${client.socketId}`).emit('callUserToClient')
+                }
+            }
         } catch (error) {
             logger.info(error.message);
         }
