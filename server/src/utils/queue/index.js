5fe92c8adf5676398dd7ad5d8faaeb3526e2d0f5
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-09-05 19:34:59 +0700

    commit

---
+++ server/src/utils/queue/index.js
@@ -0,0 +1,11 @@
+class Queue {
+    constructor() { this.q = []; }
+    items() { return this.q; }
+    enqueue(item) { this.q.push(item) }
+    dequeue() { this.q.shift(); }
+    size() { return this.q.length; }
+    deleteVideo(id) { this.q = this.q.filter((video) => video.id !== id) }
+    setItems(items) { this.q = items; }
+}
+
+module.exports = new Queue;
