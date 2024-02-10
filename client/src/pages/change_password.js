5fc6cd1801fb285894ca1d09893b7508c7369d0b
vinhngo1907 <vinhtrungngo1907@gmail.com>
2024-02-10 10:45:27 +0700

    commit

--- client/src/pages/change_password.js
+++ client/src/pages/change_password.js
@@ -11,32 +11,37 @@ const ChangePassword = () => {
             <div className="main_sidebar py-3 col-md-6">
                 <div className="central-meta">
                     <div className="editing-info">
-                        <div className="widget">
-                            <h5 className="widget-title">
-                                <i className="fas fa-lock mr-2" />
-                                Change Password
-                            </h5>
-                        </div>
+                        <h5 className="f-title">
+                            <i className="fas fa-lock" />
+                            Change Password
+                        </h5>
                         <form>
                             <div className="form-group">
-                                <input type="password" id="password" required="required" className="form-control"/>
-                                <label className="control-label" htmlFor="password">New password</label><i className="mtrl-select"></i>
+                                <input type="password" id="password" required="required" />
+                                <label className="control-label" htmlFor="password">New password</label>
+                                <i className="mtrl-select" />
                             </div>
                             <div className="form-group">
-                                <input type="password" required="required" id="confirm-pass" className="form-control"/>
-                                <label className="control-label" htmlFor="confirm-pass">Confirm password</label><i className="mtrl-select"></i>
+                                <input type="password" id="confirm-password" required="required" />
+                                <label className="control-label" htmlFor="confirm-password">Confirm password</label>
+                                <i className="mtrl-select" />
                             </div>
                             <div className="form-group">
-                                <input type="password" required="required" id="current-pass" className="form-control"/>
-                                <label className="control-label" htmlFor="current-pass">Current password</label><i className="mtrl-select"></i>
+                                <input type="password" required="required" id="current-pass" />
+                                <label className="control-label" htmlFor="current-pass">Current password</label>
+                                <i className="mtrl-select" />
                             </div>
                             <Link
                                 className="forgot-pwd underline"
                                 to="#">Forgot Password?
                             </Link>
                             <div className="submit-btns">
-                                <button type="button" className="mtr-btn"><span>Cancel</span></button>
-                                <button type="button" className="mtr-btn"><span>Update</span></button>
+                                <button type="button" className="mtr-btn">
+                                    <span>Cancel</span>
+                                </button>
+                                <button type="button" className="mtr-btn">
+                                    <span>Update</span>
+                                </button>
                             </div>
                         </form>
                     </div>
