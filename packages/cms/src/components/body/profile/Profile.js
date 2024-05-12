54fde18abb7454fc42dd3f2e547c2d3dadcab805
Koo Kuu <vinhtrungngo1907@gmail.com>
2024-05-12 10:36:35 +0700

    commit

--- packages/cms/src/components/body/profile/Profile.js
+++ packages/cms/src/components/body/profile/Profile.js
@@ -1,5 +1,6 @@
-import React from "react";
-import {useDispatch, useSelector} from "react-redux";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { showSuccessMsg, showErrMsg } from '../../utils/notifications/Notification'
 
 const initialState = {
     name: "",
@@ -10,9 +11,66 @@ const initialState = {
 }
 
 const Profile = () => {
-    const {} = useSelector
+    const { auth, token, users } = useSelector(state => state);
+    const [avatar, setAvatar] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [callback, setCallback] = useState(false);
+    const { user, isAdmin } = auth;
+    const [data, setData] = useState(initialState);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setData({ ...data, [name]: value, err: '', success: '' });
+    }
+
+    const handleChangeAvatar = async (e) => {
+        e.preventDefault();
+        try {
+
+        } catch (error) {
+            setData({ ...data, err: "", success: "" });
+        }
+    }
+
     return (
-        <></>
+        <>
+            <div>
+                {err && showErrMsg(err.message)}
+                {success && showSuccessMsg(success.message)}
+                {loading && <h3>Loading...</h3>}
+            </div>
+            <div className="profile_page">
+                <div className="col-left">
+                    <h2>{isAdmin ? "Admin Profile" : "User Profile"}</h2>
+                    <div className="avatar">
+                        {/* <img src={avatar ? avatar : user.avatar} alt="" /> */}
+                        <span>
+                            <i className="fas fa-camera"></i>
+                            <p>Change</p>
+                            <input type="file" name="file" id="file_up" onChange={changeAvatar} />
+                        </span>
+                    </div>
+
+                </div>
+                <div className="col-right">
+                    <h2>{isAdmin ? "Users" : "Admin"}</h2>
+                    <div style={{ overflowX: "auto" }}>
+                        <table className="customers">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Admin</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody></tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
