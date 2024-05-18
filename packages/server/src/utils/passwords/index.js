4d7953aae2e6fdb9ca2c3d6095e1b66edfbf3e8a
Koo Kuu <vinhtrungngo1907@gmail.com>
2024-05-18 15:21:59 +0700

    check root

--- packages/server/src/utils/passwords/index.js
+++ packages/server/src/utils/passwords/index.js
@@ -1,20 +1,44 @@
 const bcrypt = require("bcrypt");
+const { scrypt, randomBytes } = require('crypto');
 
 class Password {
     async GenerateSalt() {
         return await bcrypt.genSalt();
     }
-    
+
     async GeneratePassword(password, salt) {
         return await bcrypt.hash(password, salt);
     }
-    
+
     async ValidatePassword(
         enteredPassword,
         savedPassword,
         salt
     ) {
         return (await this.GeneratePassword(enteredPassword, salt) === savedPassword);
+    }
+    async ToHash(password) {
+        const salt = randomBytes(8).toString('hex');
+        const buf = await new Promise((resolve, reject) => {
+            scrypt(password, salt, 64, (err, derivedKey) => {
+                if (err) reject(err);
+                else resolve(derivedKey);
+            });
+        });
+
+        return `${buf.toString('hex')}.${salt}`;
+    }
+
+    async Compare(storedPassword, suppliedPassword) {
+        const [hashedPassword, salt] = storedPassword.split('.');
+        const buf = await new Promise((resolve, reject) => {
+            scrypt(suppliedPassword, salt, 64, (err, derivedKey) => {
+                if (err) reject(err);
+                else resolve(derivedKey);
+            });
+        });
+
+        return buf.toString('hex') === hashedPassword;
     }
 }
 
