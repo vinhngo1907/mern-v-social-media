6538a0624a88a65f5c0659e67ffc34ed57924121
Koo Kuu <vinhtrungngo1907@gmail.com>
2025-09-05 09:10:37 +0700

    update - upgrade

--- packages/server/src/db/connection.db.js
+++ packages/server/src/db/connection.db.js
@@ -4,10 +4,12 @@ const { DB_URL } = require("../configs");
 Promise = global.Promise;
 
 const mongoConfig = {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
+    // useCreateIndex: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // useFindAndModify: false
+    serverSelectionTimeoutMS: 5000,
+    maxPoolSize: 10,
 };
 
 module.exports = async () => {
